a5144b7ebc1f21aa4b0356b60b8c65ba1342c64b
argus portal <argusportal@gmail.com>
2024-04-28 14:39:38 -0300

    atualizadno arquivo

--- curso1/app.py
+++ curso1/app.py
@@ -1,4 +1,7 @@
-print("""
+import os
+
+def exibir_nome_programa():
+    print("""
 
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -8,10 +11,50 @@ print("""
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
 """)
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: \n')
-print(f'Você escolheu a opção {opcao_escolhida}')
+def finalizar_app():
+    os.system('clear')
+    print('Finalizando o app\n')
+
+def escolher_opcao():    
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    print(f'\nVocê escolheu a opção {opcao_escolhida}')
+
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurante')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    elif opcao_escolhida == 4:
+        finalizar_app()
+    else:
+        print('Opção Inválida ')
+
+def escolher_opcao2():
+    opcao_escolhida2 = int(input('Escolha uma opção: '))
+    match opcao_escolhida2:                                   # alternativa ao uso do if
+        case 1:
+            print('Adicionar restaurante')
+        case 2:
+            print('Listar restaurantes')
+        case 3:
+            print('Ativar restaurante')
+        case 4:
+            print('Finalizar app')
+        case _:
+            print('Opção inválida!')
+
+
+def main():
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()    
