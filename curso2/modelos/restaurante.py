173fa9e52ea0e50bd9e909f8da67219803d61ac6
argus portal <argusportal@gmail.com>
2024-04-28 17:14:12 -0300

    curso 2

--- curso2/modelos/restaurante.py
+++ curso2/modelos/restaurante.py
@@ -1,11 +1,15 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
+
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
     
-restaurante_praca = Restaurante()
-restaurante_pizza = Restaurante()
-
-restaurantes = [restaurante_praca, restaurante_pizza]
-
-print(restaurantes)
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
