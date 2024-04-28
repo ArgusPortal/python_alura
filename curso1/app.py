10472fc4de7f15cc752b8647d5e14c819dc47dc6
argus portal <argusportal@gmail.com>
2024-04-28 15:28:01 -0300

    atualizadno arquivo

--- curso1/app.py
+++ curso1/app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ['Pizza', 'Sushi']
+
 def exibir_nome_programa():
     print("""
 
@@ -21,37 +23,66 @@ def finalizar_app():
     os.system('clear')
     print('Finalizando o app\n')
 
+def opcao_invalida():
+    print('Opcao Inválida\n')
+    input('Digite uma tecla para voltar ao menu principal ')
+    main()
+
+def cadastrar_novo_restaurante():
+    os.system('clear')
+    print('Cadastro de novos restaurantes\n')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com Sucesso !\n')
+    input('\nDigite uma tecla para voltar ao menu principal\n')
+    main()
+
+def Listar_restaurantes():
+    os.system('clear')
+    print('Listando os restaurantes\n')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    input('\nDigite uma tecla para voltar ao menu principal\n')
+    main()
+
 def escolher_opcao():    
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    print(f'\nVocê escolheu a opção {opcao_escolhida}')
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        print(f'\nVocê escolheu a opção {opcao_escolhida}')
 
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurante')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurante')
-    elif opcao_escolhida == 4:
-        finalizar_app()
-    else:
-        print('Opção Inválida ')
-
-def escolher_opcao2():
-    opcao_escolhida2 = int(input('Escolha uma opção: '))
-    match opcao_escolhida2:                                   # alternativa ao uso do if
-        case 1:
-            print('Adicionar restaurante')
-        case 2:
-            print('Listar restaurantes')
-        case 3:
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2:
+            Listar_restaurantes()
+        elif opcao_escolhida == 3:
             print('Ativar restaurante')
-        case 4:
-            print('Finalizar app')
-        case _:
-            print('Opção inválida!')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
+
+# def escolher_opcao2():
+    # opcao_escolhida2 = int(input('Escolha uma opção: '))
+    # match opcao_escolhida2:                                   # alternativa ao uso do if
+    #     case 1:
+    #         print('Adicionar restaurante')
+    #     case 2:
+    #         print('Listar restaurantes')
+    #     case 3:
+    #         print('Ativar restaurante')
+    #     case 4:
+    #         print('Finalizar app')
+    #     case _:
+    #         print('Opção inválida!')
 
 
 def main():
+    os.system('clear')
     exibir_nome_programa()
     exibir_opcoes()
     escolher_opcao()
